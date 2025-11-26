8a8750927e80bfa26e41e4f5711fb5e3567732f5
OOOAkeaDS2025 <gal@akea-ds.ru>
2025-11-26 11:32:57 +0400

    Добавил Advertisements и Category

--- advertisements/models.py
+++ advertisements/models.py
@@ -1,3 +1,27 @@
+from tabnanny import verbose
+from unicodedata import category
 from django.db import models
+from django.db.models.functions import Length
 
-# Create your models here.
+
+class Category(models.Model):
+  title = models.CharField(max_length=50, verbose_name="Категория")
+  class Meta:
+    verbose_name = 'Категория'
+    verbose_name_plural = 'Категории'
+    
+  def __str__(self):
+    return self.title
+class Advertisements(models.Model):
+  title = models.CharField(max_length=100, verbose_name="Заголовок")
+  text = models.TextField(verbose_name="Текст")
+  created_at = models.DateTimeField(auto_now_add=True)
+  isActive = models.BooleanField(verbose_name="Объявление активно")
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+
+  class Meta:
+    verbose_name = 'Объявление'
+    verbose_name_plural = 'Объявления'
+
+  def __str__(self):
+    return self.title
